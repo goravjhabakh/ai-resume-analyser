a4db29e20bab45c1a69faadd7dedcb3287368d52
goravjhabakh <goravjhabakh@gmail.com>
2025-11-18 17:36:07 +0530

    Upload resume and storing in puter

---
+++ app/components/FileUploader.tsx
@@ -0,0 +1,69 @@
+import { useCallback, useState } from "react"
+import { useDropzone } from "react-dropzone"
+import { formatSize } from "~/lib/utils"
+
+interface FileUploaderProps {
+  onFileSelect?: (file: File | null) => void
+}
+
+const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const file = acceptedFiles[0] || null;
+
+    onFileSelect?.(file)
+  }, [onFileSelect])
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({ 
+    onDrop, 
+    multiple: false,
+    accept: { 'application/pdf': ['.pdf'] },
+    maxSize: 20 * 1024 * 1024,  // 20 MB
+  });
+
+  const file = acceptedFiles[0] || null;
+
+  return (
+    <div className="w-full gradient-border">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+
+        <div className="space-y-4 cursor-pointer">
+          <div className="mx-auto w-16 h-16 flex items-center justify-center">
+            <img src="/icons/info.svg" alt="upload" className="size-20"/>
+          </div>
+
+          {file ? (
+            <div className="uploader-selected-file" onClick={(e) => e.stopPropagation()}>
+              <img src="/images/pdf.png" alt="pdf" className="size-10"/>
+
+              <div className="flex items-center space-x-3">
+                <div>
+                  <p className="text-sm font-medium text-gray-700 truncate max-w-xs">
+                    {file.name}
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    {formatSize(file.size)}
+                  </p>
+                </div>
+              </div>
+
+              <button type="button" className="p-2 cursor-pointer" onClick={() => {onFileSelect?.(null)}}>
+                <img src="/icons/cross.svg" alt="remove" className="size-4"/>
+              </button>
+            </div>
+          ): (
+            <div>
+              <p className="text-lg text-gray-500">
+                <span className="font-semibold">
+                  Click to Upload
+                </span> or drag 'n' drop
+              </p>
+              <p className="text-lg text-gray-500">PDF (max 20MB)</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FileUploader
--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -9,7 +9,7 @@ export const meta = () => {
   ];
 };
 
-const auth = () => {
+const Auth = () => {
   const { isLoading, auth } = usePuterStore();
   const location = useLocation();
   const next = location.search.split('next=')[1];
@@ -50,4 +50,4 @@ const auth = () => {
   )
 }
 
-export default auth
+export default Auth

ed84a185e33e18b92c1ec7fd845b153f98c43afa
goravjhabakh <goravjhabakh@gmail.com>
2025-11-18 13:44:38 +0530

    Home Page UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "constants/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeDex" },
+    { name: "description", content: "Smart Resume Analyser" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar/>
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track your applications and resume ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume: Resume) => (
+              <div>
+                <ResumeCard key={resume.id} resume={resume}/>
+              </div>
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  )
 }
